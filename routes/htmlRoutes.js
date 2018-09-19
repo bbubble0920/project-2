57fa168ea8bdfbc96d7008344bf2772178998f40
Matthew Tai <matthewtai@gmail.com>
2018-09-19 04:15:21 -0400

    HANDLEBARSSS

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,16 @@
 var express = require("express"); 
 var router = express.Router(); 
 
+// Import model (models.js) to use database functions
+var wwdb = require('../models/models.js');
+
 // GET HOMEPAGE
 router.get("/", function(req, res){
-    res.render("login"); 
+    res.render("index"); 
+});
+
+router.get("/artist/:id", function(req, res){
+    res.render("artistpage"); 
 });
 
 module.exports = router; 
